fe1e00f09de3e158536337e3432fb0284d4dad8a
Jungkyun Woo <73513965+woo-jk@users.noreply.github.com>
2021-05-27 06:05:11 +0900

    Add files via upload

---
+++ receiver_201702042.py
@@ -0,0 +1,63 @@
+import socket
+import os
+import sys
+import hashlib
+
+def check_md5_hash(path):
+    f = open(path, 'rb')
+    data = f.read()
+    md5_hash = hashlib.md5(data).hexdigest()
+    return md5_hash
+
+
+host = "192.168.219.111"
+port = 8000
+
+if __name__=='__main__':
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setblocking(0)
+        s.settimeout(15)
+        print("receiver created")
+   
+    except socket.error:
+        print("1failed to create socket")
+        sys.exit()
+    
+    while True:
+        command = input("enter a command: \n1. receive [file_name]\n2. exit\n")
+        command_spl = command.split()
+
+        if command_spl[0] == 'exit':
+            socket.close()
+            sys.exit()
+
+        if command_spl[0] == 'receive':
+            s.sendto(command.encode(), (host, int(port)))
+
+            is_valid, addr = s.recvfrom(2000)
+            print(is_valid.decode())
+            is_exist, addr = s.recvfrom(2000)
+            print(is_exist.decode())
+            file_size, addr = s.recvfrom(2000)
+            print(file_size.decode())
+            num, addr = s.recvfrom(2000)
+
+            num = int(num.decode()) + 1
+            check = num
+
+            write_file = open(command_spl[1], "wb")
+
+            while check != 1:
+                chunk_file, addr = s.recvfrom(2048)
+                write_file.write(chunk_file)
+                packet_msg = "packet number " + str(num - check + 1)
+                print(packet_msg)
+                check -= 1
+    
+            write_file.close()
+        else:
+            print("Command is wrong")
+            socket.close()
+            sys.exit()
+
---
+++ sender_201702042.py
@@ -0,0 +1,66 @@
+import socket
+import os
+import sys
+import hashlib
+
+addr = '10.211.55.5'
+port = 8000
+
+try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind(('', 8000))
+        print("waiting for client.")
+except socket.error:
+	print("failed to create socket")
+	sys.exit()
+
+def check_md5(path):
+	f = open(path, 'rb')
+	data = f.read()
+	md5_hash = hashlib.md5(data).hexdigest()
+	return md5_hash
+
+def sender_send(file_name, addr):
+        s.sendto("valid list command.".encode(), addr)
+        if os.path.isfile(file_name):
+            s.sendto("file exists!".encode(), addr)
+
+            file_size = os.stat(file_name).st_size
+            num = int(file_size/2048) + 1
+            size_msg = "file_size : "  + str(num)
+            s.sendto(size_msg.encode(), addr)
+            s.sendto(str(num).encode(), addr)
+            
+            read_file = open(file_name, 'rb')
+            check = num
+
+            while check != 0:
+                chunk_file = read_file.read(2048)
+                s.sendto(chunk_file, addr)
+                packet_msg = "packet number " + str(num - check + 1)
+                print(packet_msg)
+                check -= 1
+            read_file.close()
+            print("send complete")
+
+        else: 
+            print("file not found")
+
+
+if __name__ == "__main__":
+    while True:    
+        try:
+            data, client_addr = s.recvfrom(2000)
+        except ConnectionResetError:
+                print("error. port number not matching.")
+                sys.exit()
+
+        text = data.decode('utf8')
+        handler = text.split()
+
+        if handler[0] == 'receive':
+            sender_send(handler[1], client_addr)
+        else:
+            print("Command is wrong")
+            s.close()
+            sys.exit()
